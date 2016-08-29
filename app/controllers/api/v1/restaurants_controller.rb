490ecadc9a2ed2024a093ecf830c5266739df677
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-29 14:59:05 +0200

    adding pundit

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+
+  before_action :set_restaurant, only: [ :show ]
+
   def index
     @restaurants = policy_scope(Restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit
   end
 end
